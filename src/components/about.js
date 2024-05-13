3b14c73a5e7c68512d28ccc3318184ce8fde64f6
khyledhanani <khyledhanani@gmail.com>
2024-05-13 03:35:10 -0400

    lgtm

--- src/components/about.js
+++ src/components/about.js
@@ -5,28 +5,24 @@ function About() {
         <div id="about" className="about-container">
             <hr></hr>
             <h2>About Me</h2>
-            <img className="about-image" src="/headshot.png" alt="person"  />
+            <img className="about-image" src="/headshot.png" alt="person" />
             <p className="about-me">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam nulla facilisi cras fermentum odio
-                eu. Habitant morbi tristique senectus et netus et malesuada fames. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Turpis egestas pretium
-                aenean pharetra. Scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada. Odio ut sem nulla pharetra diam sit
-                amet. Vitae nunc sed velit dignissim. Amet nulla facilisi morbi tempus iaculis urna id volutpat. Et netus et malesuada fames
-                ac turpis egestas maecenas pharetra. In ornare quam viverra orci sagittis eu volutpat. Eu non diam phasellus vestibulum lorem. Nulla aliquet porttitor lacus luctus
-                accumsan tortor posuere ac ut. Risus nec feugiat in fermentum posuere urna. Sed libero enim sed faucibus. Vitae semper
-                quis lectus nulla at volutpat diam. Elit pellentesque habitant morbi tristique. At erat pellentesque adipiscing commodo. Lectus mauris ultrices eros in cursus turpis. Mauris sit amet massa vitae tortor condimentum lacinia quis vel. Dolor morbi non arcu risus quis varius quam quisque. In cursus turpis massa tincidunt dui ut ornare lectus. Nunc eget lorem dolor sed viverra ipsum nunc aliquet bibendum. Pretium fusce id velit ut. Ipsum dolor sit amet consectetur adipiscing elit. Massa sed elementum tempus egestas sed sed risus pretium quam.
+                Welcome to my personal website! I created this to be a place where I can house all of my projects and future blog posts about interesting topics that I have learned about in the past.
+
+                A little bit about me: I am a 5th year Math student at the University of Waterloo specializing in Mathematics/Financial Analysis and Risk Management and minoring in Computing.
+                I have prior experience in capital markets from working at Manulife, TD Securities and TD Asset Management.
+                I also have experience in data analytics from working at Daisy Intelligence.
             </p>
             <div className="experience-projects-container">
                 <div className="work-experience">
                     <h3>Most Recent Work Experience @ Manulife</h3>
-                    <p id="projects">Recently, I worked at Manulife Investment Management within the fixed income reporting group. Most of the work that I did was
-                        related to their fixed income reporting data pipeline. I worked on a few projects that involved
-                        using machine learning models to track yield curve movements. I also helped with the improvement of a few internal python scripts that automated
-                        data retreival from the bloomberg terminal. For more info please see my resume... 
+                    <p id="projects">
+                        Recently, I worked at Manulife Investment Management within the fixed income reporting group. Most of the work that I did was related to their fixed income reporting data pipeline. I worked on a few projects that involved using machine learning models to track yield curve movements. I also helped with the improvement of a few internal python scripts that automated data retrieval from the Bloomberg terminal. For more info please see my resume...
                     </p>
                 </div>
             </div>
         </div>
-    )
+    );
     }
 
 export default About;
